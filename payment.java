98bae3df2089b706549d7ba30cc155da0d8bfa0e
Sagar Gupta <sagar03.gupta@gmail.com>
2020-05-24 23:52:43 +0530

    New development in payment.java in Develop branch

--- payment.java
+++ payment.java
@@ -1,22 +1,4 @@
 payment feature
 
 
-fix for production issue
-
-changes in file for new  feature
-
-merged prod fix with  first stash
-
-merged prod fix and  new feature code  for payments
-
-###
-
-new changes 2020 in payment functionality - discarded
-
-new patch in payment module 2020
-
-new debit card feature chagnes in payment module a s well
-
-
-#############
-new changes after new use pull the git repostiry
+Deveoper is working on new enhancement on Payment.java
